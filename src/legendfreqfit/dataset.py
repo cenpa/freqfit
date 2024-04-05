b099b478bf368f9b2f92db1315d2dd2efcd2e919
lvarriano <lvarriano@anl.gov>
2024-04-05 12:22:00 -0700

    add annotation

--- src/legendfreqfit/dataset.py
+++ src/legendfreqfit/dataset.py
@@ -1,5 +1,5 @@
 """
-A class that holds a dataset and an associated model,
+A class that holds a dataset and its associated model and cost function,
 """
 
 import warnings
@@ -8,7 +8,7 @@ from iminuit import cost, util
 import inspect
 import typing
 
-class dataset:
+class Dataset:
     def __init__(
         self, 
         data:np.array,
@@ -19,11 +19,50 @@ class dataset:
         """
         Parameters
         ----------
+        data
+            D `ndarray` of the data, which should be a list of energies
         model
-            a model class that has some particular functions
-        kwmap
-            a dictionary for this dataset that maps the aribitrary variable names to
-            the variable names that the model is expecting.
+            callable model to be passed to the cost function (e.g. `gaussian_on_uniform.density`).`model`
+            must have a callable form `model(data, a, b, c, ...)` where `data` takes a 1D `ndarray` corresponding
+            to unbinned events, and `a, b, c,...` are the parameters of the model, which may take any type and may include
+            default values. `model` must return the form expected by `costfunction`.
+        parameters
+            `dict` for the parameters of this particular model that allow for control over how a fit is performed across
+            multiple datasets. The `dict` should have keys corresponding to each required parameter of `model` (each
+            parameter without a default value), where the key is a string of the name of each parameter, e.g. for
+            parameters `a, b, c, ...`, the keys are `"a", "b", "c", ...`.
+            
+            The value for each key should be a `dict` that can contain the following keys and values: 
+            - "name": a string containing a different name for the parameter that will be passed to `iminuit` in place
+                of the original name. This can be used to fit the same parameter across multiple datasets since
+                `iminuit` references variables by name. This key is optional and the default parameter name from `model`
+                will be used if "name" is not found.
+
+            - "includeinfit": True or False, corresponding to whether this parameter should be included in the `iminuit`
+                fit or if its value should be fixed. This allows for passing variables of any type to the underlying
+                `model`. If True, no "value" will be passed to `model` for this parameter and will instead be taken
+                from `iminuit` during the minimization. If "False", then "value" is required if the parameter does not
+                have a default value and will be passed to `model`. Note that it is still possible to include 
+                parameters in the `iminuit` minimization and set them to `fixed`, which can be desireable in some cases.
+            
+            - "value": If "includeinfit" is `False` and no default value for the parameter is specified in `model`, then
+                this is required. This may be any type and will be passed to `model`. If "includeinfit" is `True`, this
+                is not used. If "includeinfit" is `False` and a default value for the parameter exists, this will 
+                overwrite the default.
+            
+            - "limits": This allows for setting limits on the parameters for `iminuit`. It is still possible to set
+                limits through `iminuit` for parameters, but this key can be convenient for parameters not shared 
+                between `Dataset`. Note that for parameters shared across several `Dataset`, limits specified for 
+                such a parameter may be overwritten if a different limit is specified in a different `Dataset` whose
+                cost functions are then added. 
+
+        costfunction
+            an `iminuit` cost function. Currently, only `cost.ExtendedUnbinnedNLL` or `cost.UnbinnedNLL` are supported 
+            as cost functions.
+            
+        Notes
+        -----
+        Currently, only `cost.ExtendedUnbinnedNLL` or `cost.UnbinnedNLL` are supported as cost functions.
         """
 
         self.data = data
@@ -103,7 +142,7 @@ class dataset:
         # assign the positional parameters to the correct places in the model parameter list
         for i in range(len(par)):
             self._parlist[self._partofitindices[i]] = par[i]
-            
+
         return self.modelname(data, *self._parlist)
 
 # takes a model and returns a dict of its parameters with their default value
