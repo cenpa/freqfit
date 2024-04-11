373f3a92c94b7922ded24359c40b280e853a95fe
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2024-04-11 15:46:13 -0700

    Add pyyaml as dependency and export Pseudoexperiment

--- src/legendfreqfit/__init__.py
+++ src/legendfreqfit/__init__.py
@@ -3,5 +3,6 @@ A package for setting frequenstist limits from unbinned data
 """
 
 from legendfreqfit._version import version as __version__
+from legendfreqfit.pseudoexperiment import Pseudoexperiment
 
 __all__ = ["__version__"]
