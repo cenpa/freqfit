6e849d2d131172b719f78733d795478ecf63a92d
lvarriano <lvarriano@anl.gov>
2024-04-12 12:18:13 -0700

    adjust initial guess stuff

--- src/legendfreqfit/pseudoexperiment.py
+++ src/legendfreqfit/pseudoexperiment.py
@@ -31,11 +31,14 @@ class Pseudoexperiment(Superset):
         guess = {fitpar: self.parameters[fitpar]["value"]  if "value" in self.parameters[fitpar] 
                         else None for fitpar in self.fitparameters}
         
-        for fitpar, value in guess.items():
-            if value is None:
-                guess[fitpar] = 1e-9
+        # for fitpar, value in guess.items():
+        #     if value is None:
+        #         guess[fitpar] = 1e-9
         
-        # could put other stuff here to get a better initial guess
+        # could put other stuff here to get a better initial guess though probably that should be done
+        # somewhere specific to the analysis. Or maybe this function could call something for the analysis.
+        # eh, probably better to make the initial guess elsewhere and stick it in the config file.
+        # for that reason, I'm commenting out the few lines above.
 
         return guess
 
