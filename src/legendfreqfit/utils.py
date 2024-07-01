42e895610da45620fe83ef4ccc3116e9f03af7f1
lvarriano <lvarriano@anl.gov>
2024-07-01 10:12:14 -0700

    change YAML loader to detect duplicate keys

--- src/legendfreqfit/utils.py
+++ src/legendfreqfit/utils.py
@@ -44,7 +44,7 @@ def load_config(
     Loads a config file and converts `str` for some fields to the appropriate objects.
     """
     with open(file) as stream:
-        config = yaml.safe_load(stream)
+        config = yaml.load(stream, Loader=UniqueKeyLoader)
 
     # get list of models and cost functions to import
     models = set()
@@ -152,3 +152,16 @@ def dkw_band(
     lo_band = np.maximum(cdf - eps, np.zeros_like(cdf))
     hi_band = np.minimum(cdf + eps, np.ones_like(cdf))
     return lo_band, hi_band
+
+# use this YAML loader to detect duplicate keys in a config file
+# https://stackoverflow.com/a/76090386
+class UniqueKeyLoader(yaml.SafeLoader):
+    def construct_mapping(self, node, deep=False):
+        mapping = set()
+        for key_node, value_node in node.value:
+            each_key = self.construct_object(key_node, deep=deep)
+            if each_key in mapping:
+                raise ValueError(f"Duplicate Key: {each_key!r} is found in YAML File.\n"
+                                 f"Error File location: {key_node.end_mark}")
+            mapping.add(each_key)
+        return super().construct_mapping(node, deep)
