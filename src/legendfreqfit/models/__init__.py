a5c35458604dce58186c2045458638ea81ec54da
lvarriano <lvarriano@anl.gov>
2024-07-09 17:57:38 -0700

    spelled file name wrong

--- src/legendfreqfit/models/__init__.py
+++ src/legendfreqfit/models/__init__.py
@@ -5,5 +5,6 @@ Models for unbinned frequentist fitting. These models are classes and come with 
 from legendfreqfit.models.bkg_region_0vbb import bkg_region_0vbb
 from legendfreqfit.models.gaussian_on_uniform import gaussian_on_uniform
 from legendfreqfit.models.sig_region_0vbb import sig_region_0vbb
+from legendfreqfit.models.correlated_efficiency_0vbb import correlated_efficiency_0vbb
 
-__all__ = ["gaussian_on_uniform", "sig_region_0vbb", "bkg_region_0vbb"]
+__all__ = ["gaussian_on_uniform", "sig_region_0vbb", "bkg_region_0vbb", "correlated_efficiency_0vbb"]
