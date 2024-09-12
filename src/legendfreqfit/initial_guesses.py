239aba51316d96b9e6cdcb4bb8f35988b9832590
SamuelBorden <sjborden@uw.edu>
2024-09-11 21:12:40 -0700

    Made initial guesses generic for combined experiments

--- src/legendfreqfit/initial_guesses.py
+++ src/legendfreqfit/initial_guesses.py
@@ -25,66 +25,158 @@ QBB = constants.QBB
 
 
 def zero_nu_initial_guess(experiment):
-    # Loop through the datasets and grab the exposures, efficiencies, and sigma
+    # figure out if this is a toy or not:
+    if hasattr(experiment, "experiment"):
+        is_toy = True
+        loop_exp = experiment.experiment
+    else:
+        is_toy = False
+        loop_exp = experiment
+    # Loop through the datasets and grab the exposures, efficiencies, and sigma from all datasets
     totexp = 0.0
     sigma_expweighted = 0.0
     eff_expweighted = 0.0
     effunc_expweighted = 0.0
     Es = []
 
+    # Find which datasets share a background index
+    BI_list = [par for par in experiment.fitparameters if "BI" in par]
+    ds_list = []
+    ds_names = []
+
+    for BI in BI_list:
+        ds_per_BI = []
+        for ds in loop_exp.datasets.values():
+            if is_toy:
+                if (BI in ds.fitparameters) & (not ds._toy_is_combined):
+                    ds_per_BI.append(ds)
+            else:
+                if (BI in ds.fitparameters) & (not ds.is_combined):
+                    ds_per_BI.append(ds)
+
+        if is_toy:
+            for ds in experiment.combined_datasets.values():
+                if BI in ds.fitparameters:
+                    ds_per_BI.append(ds)
+        else:
+            for ds in loop_exp.combined_datasets.values():
+                if BI in ds.fitparameters:
+                    ds_per_BI.append(ds)
+
+        ds_list.append(ds_per_BI)
+
+        ds_names.append([ds.name for ds in ds_per_BI])
+
+    # Then perform the loop over datasets that share a background index
+    BI_guesses = []
+    for ds_BI in ds_list:
+        # Get estimates for these parameters based only on the datasets contributing to one BI
+        BI_totexp = 0.0
+        BI_sigma_expweighted = 0.0
+        BI_eff_expweighted = 0.0
+        BI_effunc_expweighted = 0.0
+        Es_per_BI = []
+
+        # need to handle this differently depending on if this is a toy or not/what datasets have been combined
+        for ds in ds_BI:
+            if isinstance(ds.model, correlated_efficiency_0vbb_gen):
+                BI_totexp = BI_totexp + ds._parlist[7]
+                BI_sigma_expweighted = (
+                    BI_sigma_expweighted + ds._parlist[3] * ds._parlist[7]
+                )
+                BI_eff_expweighted = (
+                    BI_eff_expweighted + ds._parlist[4] * ds._parlist[7]
+                )
+                BI_effunc_expweighted = (
+                    BI_effunc_expweighted + ds._parlist[5] * ds._parlist[7]
+                )
+                if is_toy:
+                    if ds._toy_data is not None:
+                        Es_per_BI.extend(ds._toy_data)
+                    else:
+                        Es_per_BI.extend([])
+                else:
+                    Es_per_BI.extend(ds.data)
+            elif isinstance(ds.model, mjd_0vbb_gen):
+                BI_totexp = BI_totexp + ds._parlist[10]
+                BI_sigma_expweighted = (
+                    BI_sigma_expweighted + ds._parlist[4] * ds._parlist[10]
+                )
+                BI_eff_expweighted = (
+                    BI_eff_expweighted + ds._parlist[7] * ds._parlist[10]
+                )
+                BI_effunc_expweighted = (
+                    BI_effunc_expweighted + ds._parlist[8] * ds._parlist[10]
+                )
+                if is_toy:
+                    if ds._toy_data is not None:
+                        Es_per_BI.extend(ds._toy_data)
+                    else:
+                        Es_per_BI.extend([])
+                else:
+                    Es_per_BI.extend(ds.data)
+            else:
+                raise NotImplementedError(
+                    f"Model of type {ds.model} not yet implemented here!"
+                )
+
+        totexp += BI_totexp
+        sigma_expweighted += BI_sigma_expweighted
+        eff_expweighted += BI_eff_expweighted
+        effunc_expweighted += BI_effunc_expweighted
+        Es.extend(Es_per_BI)
+
+        BI_sigma_expweighted = BI_sigma_expweighted / BI_totexp
+        BI_eff_expweighted = BI_eff_expweighted / BI_totexp
+        BI_effunc_expweighted = BI_effunc_expweighted / BI_totexp
+
+        # Finally, we are ready to make our guess for this BI
+        # If we get only one count in the signal window, then this guess will estimate too low a background
+        # So, if BI is guessed as 0 and S is not 0, smear out the signal rate between them
+        BI_guess, s_guess = guess_BI_S(
+            Es_per_BI, BI_totexp, BI_eff_expweighted, BI_sigma_expweighted
+        )
+        if s_guess < 0:
+            s_guess = 0
+        if (BI_guess < 1e-6) and (s_guess != 0):
+            BI_guess = s_guess / 2
+            s_guess /= 2
+
+        BI_guesses.append(BI_guess)
+
+    # Compute the total for the experiment, so that we can better guess an initial S value
+    sigma_expweighted = sigma_expweighted / BI_totexp
+    eff_expweighted = eff_expweighted / BI_totexp
+    effunc_expweighted = effunc_expweighted / BI_totexp
+
+    S_guess = guess_BI_S(Es, totexp, eff_expweighted, sigma_expweighted)[1]
+    if S_guess < 0:
+        S_guess = 0
+
     # need to handle this differently depending on if this is a toy or not/what datasets have been combined
-    # This is for a toy
-    if hasattr(experiment, "experiment"):
-        for ds in experiment.experiment.datasets.values():
-            if isinstance(ds.model, correlated_efficiency_0vbb_gen):
-                totexp = totexp + ds._parlist[7]
-                sigma_expweighted = sigma_expweighted + ds._parlist[3] * ds._parlist[7]
-                eff_expweighted = eff_expweighted + ds._parlist[4] * ds._parlist[7]
-                effunc_expweighted = (
-                    effunc_expweighted + ds._parlist[5] * ds._parlist[7]
-                )
-                Es.extend(ds._toy_data)
-            elif isinstance(ds.model, mjd_0vbb_gen):
-                totexp = totexp + ds._parlist[10]
-                sigma_expweighted = sigma_expweighted + ds._parlist[4] * ds._parlist[10]
-                eff_expweighted = eff_expweighted + ds._parlist[7] * ds._parlist[10]
-                effunc_expweighted = (
-                    effunc_expweighted + ds._parlist[8] * ds._parlist[10]
-                )
-                Es.extend(ds._toy_data)
-            else:
-                raise NotImplementedError(
-                    f"Model of type {ds.model} not yet implemented here!"
-                )
-
-    # This is an experiment
+    if is_toy:
+        guess = {}
+        for par in experiment.fitparameters:
+            guess |= {par: experiment.experiment._toy_parameters[par]["value"]}
     else:
-        for ds in experiment.datasets.values():
-            if isinstance(ds.model, correlated_efficiency_0vbb_gen):
-                totexp = totexp + ds._parlist[7]
-                sigma_expweighted = sigma_expweighted + ds._parlist[3] * ds._parlist[7]
-                eff_expweighted = eff_expweighted + ds._parlist[4] * ds._parlist[7]
-                effunc_expweighted = (
-                    effunc_expweighted + ds._parlist[5] * ds._parlist[7]
-                )
-                Es.extend(ds.data)
-            elif isinstance(ds.model, mjd_0vbb_gen):
-                totexp = totexp + ds._parlist[10]
-                sigma_expweighted = sigma_expweighted + ds._parlist[4] * ds._parlist[10]
-                eff_expweighted = eff_expweighted + ds._parlist[7] * ds._parlist[10]
-                effunc_expweighted = (
-                    effunc_expweighted + ds._parlist[8] * ds._parlist[10]
-                )
-                Es.extend(ds.data)
-            else:
-                raise NotImplementedError(
-                    f"Model of type {ds.model} not yet implemented here!"
-                )
+        guess = {
+            fitpar: experiment.parameters[fitpar]["value"]
+            if "value" in experiment.parameters[fitpar]
+            else None
+            for fitpar in experiment.fitparameters
+        }
 
-    sigma_expweighted = sigma_expweighted / totexp
-    eff_expweighted = eff_expweighted / totexp
-    effunc_expweighted = effunc_expweighted / totexp
+    # update the BI
+    for i, BI in enumerate(BI_list):
+        guess[BI] = BI_guesses[i] / 0.0001  # For scaled BI
 
+    # Update the signal guess
+    guess["global_S"] = S_guess / 0.01  # For scaled S in the model
+
+    return guess
+
+
+def guess_BI_S(Es, totexp, eff_expweighted, sigma_expweighted):  # noqa: N802
     """
     Give a better initial guess for the signal and background rate given an array of data
     The signal rate is estimated in a +/-5 keV window around Qbb, the BI is estimated from everything outside that window
@@ -97,7 +189,7 @@ def zero_nu_initial_guess(experiment):
         The total exposure of the experiment
     eff_expweighted
         The total efficiency of the experiment
-    sigma_tot
+    sigma_expweighted
         The total sigma of the QBB peak
     """
     QBB_ROI_SIZE = [
@@ -125,33 +217,4 @@ def zero_nu_initial_guess(experiment):
 
     s_guess = n_sig / (totexp * eff_expweighted)
 
-    # need to handle this differently depending on if this is a toy or not/what datasets have been combined
-    if hasattr(experiment, "experiment"):
-        guess = {}
-        for par in experiment.fitparameters:
-            guess |= {par: experiment.experiment._toy_parameters[par]["value"]}
-    else:
-        guess = {
-            fitpar: experiment.parameters[fitpar]["value"]
-            if "value" in experiment.parameters[fitpar]
-            else None
-            for fitpar in experiment.fitparameters
-        }
-
-    # If we get only one count in the signal window, then this guess will estimate too low a background
-    # So, if BI is guessed as 0 and S is not 0, smear out the signal rate between them
-    if s_guess < 0:
-        s_guess = 0
-
-    if (BI_guess < 1e-6) and (s_guess != 0):
-        BI_guess = s_guess / 2
-        s_guess /= 2
-
-    # update only the S and BI
-    for fitpar in guess.keys():
-        if "BI" in fitpar:
-            guess[fitpar] = BI_guess / 0.0001  # For scaled BI
-        if "global_S" in fitpar:
-            guess[fitpar] = s_guess / 0.01  # For scaled S in the model
-
-    return guess
+    return BI_guess, s_guess
