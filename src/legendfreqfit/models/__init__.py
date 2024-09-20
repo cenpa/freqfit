da87358fa871ad2d3ab55dcf72c66a1d8c5736f0
lvarriano <lvarriano@anl.gov>
2024-09-20 11:02:23 -0700

    add linear_bkg model

--- src/legendfreqfit/models/__init__.py
+++ src/legendfreqfit/models/__init__.py
@@ -6,5 +6,5 @@ from legendfreqfit.models.gaussian_on_uniform import gaussian_on_uniform
 from legendfreqfit.models.correlated_efficiency_0vbb import correlated_efficiency_0vbb
 from legendfreqfit.models.mjd_0vbb import mjd_0vbb
 from legendfreqfit.models.onebin_poisson import onebin_poisson
-
-__all__ = ["gaussian_on_uniform", "correlated_efficiency_0vbb", "mjd_0vbb", "onebin_poisson"]
+from legendfreqfit.models.linear_bkg import linear_bkg
+__all__ = ["gaussian_on_uniform", "correlated_efficiency_0vbb", "mjd_0vbb", "onebin_poisson", "linear_bkg"]
