e1fbbe5f54fd6eb0ed8e6c9033e15502147cf00a
lvarriano <lvarriano@anl.gov>
2024-09-26 13:09:54 -0700

    remove old windows

--- src/legendfreqfit/models/constants.py
+++ src/legendfreqfit/models/constants.py
@@ -13,16 +13,6 @@ WINDOW = [[1930.0, 2098.511], [2108.511, 2113.513], [2123.513, 2190.0]]
 # MJD analysis window (in keV) is slightly larger than GERDA/LEGEND and excludes an additional line
 MJD_WINDOW = [[1950.0, 2098.511], [2108.511, 2113.513], [2123.513, 2199.1], [2209.1, 2350.0]]
 
-# default analysis window (in keV) but only for background region
-# used for bkg_region_0vbb
-# includes the default analysis window except Qbb +- 25 keV
-WINDOW_BKG_REGION_0VBB = [[1930.0, 2014.0], [2064.0, 2099.0], [2109.0, 2114.0], [2124.0, 2190.0]]
-
-# default analysis window (in keV) but only for signal region
-# used for sig_region_0vbb
-# includes only Qbb +- 25 keV
-WINDOW_SIG_REGION_0VBB = [[2014.0, 2064.0]]
-
 # could use these to go a little faster?
 LOG2 = 0.69314718055994528622676398299518041312694549560546875
 SQRT2PI = 2.506628274631000241612355239340104162693023681640625
